e235140e9ea1d8d4f95215e4c0efd3290125d815
Meysam Nasiri <nasiri.contact@gmail.com>
2022-04-15 16:40:09 -0400

    Add placeholder for gallery-item

--- src/components/gallery-item.js
+++ src/components/gallery-item.js
@@ -1,59 +1,72 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import { GalleryContext } from "./app";
 
 export function GalleryItem() {
   const { id } = useParams();
   const { images } = useContext(GalleryContext);
+  const [placeholder, setPlaceholder] = useState(false);
 
-  const image = images.filter((image) => image.id == id);
+  const image = images.filter((image) => image.id === id);
   const { author, width, height, url, download_url } = image.length && image[0];
- 
+
+  const _width = Math.floor(width / 10);
+  const _height = Math.floor(height / 10);
 
   return (
-    <div>
-      <figure>
-        <img
-          alt="img"
-          srcSet={`
-              https://picsum.photos/id/${id}/985 985w,
-              https://picsum.photos/id/${id}/1285 1285w,
-              https://picsum.photos/id/${id}/1585 1585w,
-              https://picsum.photos/id/${id}/1885 1885w,
-              https://picsum.photos/id/${id}/1970 1970w,
-              https://picsum.photos/id/${id}/2048 2048w
-            `}
-        />
-        <figcaption className="author">Author: {author}</figcaption>
-      </figure>
-      <h3 className="table-caption">Image information</h3>
+    <>
+      {_width && _height && (
+        <div>
+          <div className="image-container">
+            <figure>
+              <img
+                alt="img"
+                style={placeholder ? { display: "block" } : { display: "none" }}
+                id="placeholder"
+                onMouseEnter={() => setPlaceholder(true)}
+                onMouseLeave={() => setPlaceholder(false)}
+                srcSet={`https://placebear.com/${_width}/${_height}`}
+              />
+              <img
+                alt="img"
+                style={placeholder ? { display: "none" } : { display: "block" }}
+                onMouseEnter={() => setPlaceholder(true)}
+                onMouseLeave={() => setPlaceholder(false)}
+                srcSet={`https://picsum.photos/id/${id}/${_width}/${_height}`}
+              />
+              <figcaption className="author">Author: {author}</figcaption>
+            </figure>
+          </div>
+          <h3 className="table-caption">Image information</h3>
 
-      <table className="table table-bordered col-6">
-        <tbody>
-          <tr>
-            <th scope="row">Original Resolution</th>
-            <td>
-              {width} X {height}
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Download-url</th>
-            <td>
-              <a className="no-underline" href={download_url}>
-                {download_url}
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Url</th>
-            <td>
-              <a className="no-underline" href={url}>
-                {url}
-              </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+          <table className="table table-bordered col-6">
+            <tbody>
+              <tr>
+                <th scope="row">Original Resolution</th>
+                <td>
+                  {width} X {height}
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Download-url</th>
+                <td>
+                  <a className="no-underline" href={download_url}>
+                    {download_url}
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Url</th>
+                <td>
+                  <a className="no-underline" href={url}>
+                    {url}
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
   );
 }
